b95424a2dc5590c6b649396fd5b66b88eff46a25
Rudy Crespo <rudy.crespo20@gmail.com>
2015-02-06 18:10:27 -0500

    successful circle creation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var app = app || {};
+
+app.circles = (function() {
+	var s;
+
+	return {
+		settings: {
+			theBody: $('body')
+		},
+
+		init: function() {
+			s = this.settings;
+			this.createCircles();
+		},
+
+		createCircles: function() {
+			s.theBody.on('click', function(e) {
+				var $this = $(this);
+				var $circle = $('<div/>');
+				var posX = e.pageX;
+				var posY = e.pageY;
+
+				$this.append($circle.addClass('circle').css({
+			        left: posX,
+			        top: posY
+			    }));    				
+	
+			});
+		}
+	};
+
+}());
+
+$(function() {
+	app.circles.init();
+});
